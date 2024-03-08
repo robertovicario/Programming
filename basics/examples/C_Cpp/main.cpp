a8ae1d043f47f747145823fa6b859488f0bf46da
Roberto Vicario <rvicariodev@gmail.com>
2024-03-08 08:59:39 +0100

    updates

---
+++ basics/examples/C_Cpp/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void than_five() {
+    cout << "Enter a number: ";
+    int num; cin >> num;
+
+    if (num > 5) {
+        cout << num << " is greater than 5" << endl;
+    } else if (num < 5) {
+        cout << num << " is less than 5" << endl;
+    } else {
+        cout << num << " is equal to 5" << endl;
+    }
+}
+
+int main() {
+    than_five();
+
+    return 0;
+}
